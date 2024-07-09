a15592b25f925eaa481e08072b3f59cbf478b4dc
ibtissammanadir <ibtissammanadir4@gmail.com>
2024-07-09 12:08:10 +0100

    first commmit

---
+++ src/main/java/com/nacer/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.nacer.repository;
+
+import com.nacer.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart,Long> {
+
+    public Cart findByCustomerId(Long userId);
+}
